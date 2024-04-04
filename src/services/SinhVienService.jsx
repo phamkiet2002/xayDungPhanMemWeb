1a759bf5f25c6f57b6915de96570d00e046ebef0
phamkiet2002 <pham.t.kiet02022002@gmail.com>
2024-04-05 00:29:48 +0700

    moi

--- src/services/SinhVienService.jsx
+++ src/services/SinhVienService.jsx
@@ -1,10 +1,22 @@
 import axios from 'axios';
 
 
-const URL_BASE = 'http://localhost:8080/api/sinhvien';
+const URL_BASE = 'http://localhost:8080/api/hocvien';
 
 const fetchAllSinhVien = () => {
   return axios.get(URL_BASE);
 }
 
-export {fetchAllSinhVien}
+const DeleteSv = (idsinhvien) => {
+  return axios.delete(URL_BASE + '/' + idsinhvien);
+}
+
+const addSv = (sv) => {
+  return axios.post(URL_BASE, sv);
+}
+
+const updateSv = (idsinhvien, sv) => {
+  return axios.put(URL_BASE + '/' + idsinhvien, sv);
+}
+
+export { fetchAllSinhVien, DeleteSv, addSv, updateSv }
